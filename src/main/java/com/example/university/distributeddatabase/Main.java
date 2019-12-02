436a5042f4901c3c4416683ee489ba38861f5cd8
mahdi <tamahdi73@gmail.com>
2019-12-02 21:48:34 +0330

    1

--- src/main/java/com/example/university/distributeddatabase/Main.java
+++ src/main/java/com/example/university/distributeddatabase/Main.java
@@ -6,6 +6,8 @@ import com.example.university.distributeddatabase.util.Utils;
 import java.util.ArrayList;
 
 public class Main {
+
+
     public static void main(String[] args) {
         ArrayList<Integer> unsortedArray = Utils.getRandomNumbers();
         MergeSort ms = new MergeSort(unsortedArray);
