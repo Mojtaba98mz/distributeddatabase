ea04bbe688292533d759a682492261c9b4029e85
mahdi <tamahdi73@gmail.com>
2019-12-03 23:04:17 +0330

    1

--- src/main/java/com/example/university/distributeddatabase/PartOFList.java
+++ src/main/java/com/example/university/distributeddatabase/PartOFList.java
@@ -7,27 +7,14 @@ import java.util.concurrent.Callable;
 
 public class PartOFList implements Callable<MergeSort> {
     public List<Integer> unSortList;
-    public int threadCount=0;
 
-    public  PartOFList(List<Integer> unSortList,int threadCount) {
+    public  PartOFList(List<Integer> unSortList) {
         this.unSortList = unSortList;
-        this.threadCount = threadCount;
     }
-
-//    @Override
-//    public void run() {
-//        System.out.println("t"+threadCount +"===>start");
-//        MergeSort ms = new MergeSort(unSortList);
-//        ms.sortGivenArray();
-//        System.out.println("t"+threadCount +"===>end");
-//    }
-
     @Override
-    public MergeSort call() throws Exception {
-        System.out.println("t"+threadCount +"===>start");
+    public MergeSort call() {
         MergeSort ms = new MergeSort(unSortList);
         ms.sortGivenArray();
-        System.out.println("t"+threadCount +"===>end");
         return ms;
     }
 }
--- src/test/java/com/example/university/distributeddatabase/TestExecutor.java
+++ src/test/java/com/example/university/distributeddatabase/TestExecutor.java
@@ -22,12 +22,8 @@
 
 package com.example.university.distributeddatabase;
 
-import com.example.university.distributeddatabase.util.Executor;
 import com.example.university.distributeddatabase.util.MergeKSortedArrays;
-import com.example.university.distributeddatabase.util.MergeSort;
 import com.example.university.distributeddatabase.util.Utils;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -38,68 +34,29 @@ import java.util.concurrent.*;
 public class TestExecutor {
 
     @Test
-    public void InvokeAllDemo() throws InterruptedException {
-        List<Future<MergeSort>> futures;
-        System.out.println("creating service");
-        ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        ArrayList<Integer> unsortedArray = Utils.getRandomNumbers();
-        ArrayList<List<Integer>> arrayLists = new ArrayList<>();
-        arrayLists.add(unsortedArray.subList(0, 250));
-        arrayLists.add(unsortedArray.subList(250, 500));
-        arrayLists.add(unsortedArray.subList(500, 750));
-        arrayLists.add(unsortedArray.subList(750, 1000));
-        List<PartOFList> futureList = new ArrayList<PartOFList>();
-        int i=1;
-        for (List<Integer> arrayList : arrayLists) {
-            PartOFList myCallable = new PartOFList(arrayList, i);
-            futureList.add(myCallable);
-            i++;
-        }
-        System.out.println("Start");
+    public void InvokeAllDemo() {
         try {
-            futures= service.invokeAll(futureList);
-        } catch (Exception err) {
-            err.printStackTrace();
+            ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            int availableProcessors = Runtime.getRuntime().availableProcessors();
+            ArrayList<Integer> unsortedArray = Utils.getRandomNumbers();
+            ArrayList<List<Integer>> arrayListsDivided = new ArrayList<>();
+            int gam = unsortedArray.size() / availableProcessors;
+            for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+                arrayListsDivided.add(i, unsortedArray.subList(gam * i, gam * (i + 1)));
+            }
+            List<PartOFList> futureList = new ArrayList<>();
+            for (List<Integer> arrayList : arrayListsDivided) {
+                futureList.add(new PartOFList(arrayList));
+            }
+            service.invokeAll(futureList);
+            MergeKSortedArrays tester = new MergeKSortedArrays();
+            int[][] input = new int[availableProcessors][availableProcessors];
+            for (int j = 0; j < Runtime.getRuntime().availableProcessors(); j++) {
+                input[j] = Utils.toIntArray(arrayListsDivided.get(j));
+            }
+            System.out.println(Arrays.toString(tester.mergeKSortedArrays(input)));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        System.out.println("Completed");
-        MergeKSortedArrays tester = new MergeKSortedArrays();
-        int[] a1 = Utils.toIntArray(arrayLists.get(0));
-        int[] a2 = Utils.toIntArray(arrayLists.get(1));
-        int[] a3 = Utils.toIntArray(arrayLists.get(2));
-        int[] a4 = Utils.toIntArray(arrayLists.get(3));
-        int[][] input = {a1, a2, a3, a4};
-        System.out.println(Arrays.toString(tester.mergeKSortedArrays(input)));
-    }
-
-
-    @Test
-    public void test_runIncreasePoolSize() throws Exception {
-        ArrayList<Integer> unsortedArray = Utils.getRandomNumbers();
-        ArrayList<List<Integer>> arrayLists = new ArrayList<>();
-        arrayLists.add(unsortedArray.subList(0, 250));
-        arrayLists.add(unsortedArray.subList(250, 500));
-        arrayLists.add(unsortedArray.subList(500, 750));
-        arrayLists.add(unsortedArray.subList(750, 1000));
-        ExecutorService executor = Executors.newFixedThreadPool(
-                Runtime.getRuntime().availableProcessors() + 1);
-        int i = 1;
-        for (List<Integer> arrayList : arrayLists) {
-            executor.submit(
-                    new PartOFList(arrayList, i));
-            i++;
-//            executor.invokeAll();
-        }
-        int[][] result;
-        MergeKSortedArrays tester = new MergeKSortedArrays();
-        int[] a1 = Utils.toIntArray(arrayLists.get(0));
-        int[] a2 = Utils.toIntArray(arrayLists.get(1));
-        int[] a3 = Utils.toIntArray(arrayLists.get(2));
-        int[] a4 = Utils.toIntArray(arrayLists.get(3));
-        int[][] input = {a1, a2, a3, a4};
-        executor.submit(() -> {
-            int[] ints = tester.mergeKSortedArrays(input);
-            System.out.println(Arrays.toString(ints));
-        });
     }
 }
