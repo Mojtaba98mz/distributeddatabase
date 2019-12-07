2f48a7538659b9a665032e6a6096652f8a7eaed8
mojtaba <mojtaba98.mz@gmail.com>
2019-12-07 21:38:00 +0330

    bazam befarma

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,39 +1,54 @@
+var baseUrl = "http://localhost:8080/";
 $('#fetchData').on('click', function (event) {
-    $.get("http://localhost:8080/mergeAllSort?core=4", function (data, status) {
-        console.log(data);
+    let coreNumber = $('#number').val();
+    let algorithm = $("input[name='algorithm']:checked").val();
+    $.get(baseUrl + algorithm + "?core=" + coreNumber, function (data, status) {
+        showChart(data);
     });
 });
 
 
-let showChart = () => {
+let showChart = (dataSource) => {
     $("#chart").dxChart({
-        palette: "soft",
         dataSource: dataSource,
-        commonSeriesSettings: {
-            barPadding: 0.5,
-            argumentField: "state",
-            type: "bar"
-        },
-        series: [
-            {valueField: "year1970", name: "1970"},
-            {valueField: "year1980", name: "1980"},
-            {valueField: "year1990", name: "1990"},
-            {valueField: "year2000", name: "2000"},
-            {valueField: "year2008", name: "2008"},
-            {valueField: "year2009", name: "2009"}
-        ],
-        legend: {
-            verticalAlignment: "bottom",
-            horizontalAlignment: "center"
-        },
-        "export": {
-            enabled: true
-        },
-        title: {
-            text: "Oil Production",
-            subtitle: {
-                text: "(in millions tonnes)"
-            }
+        series: {
+            argumentField: "coreNumber",
+            valueField: "executionTime",
+            name: "My oranges",
+            type: "bar",
+            color: '#ffaa66'
         }
     });
 };
+
+
+/*$("#chart").dxChart({
+    palette: "soft",
+    dataSource: dataSource,
+    commonSeriesSettings: {
+        barPadding: 0.5,
+        argumentField: "state",
+        type: "bar"
+    },
+    series: [
+        {valueField: "year1970", name: "1970"},
+        {valueField: "year1980", name: "1980"},
+        {valueField: "year1990", name: "1990"},
+        {valueField: "year2000", name: "2000"},
+        {valueField: "year2008", name: "2008"},
+        {valueField: "year2009", name: "2009"}
+    ],
+    legend: {
+        verticalAlignment: "bottom",
+        horizontalAlignment: "center"
+    },
+    "export": {
+        enabled: true
+    },
+    title: {
+        text: "Oil Production",
+        subtitle: {
+            text: "(in millions tonnes)"
+        }
+    }
+});*/
